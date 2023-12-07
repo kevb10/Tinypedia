898896ec4d3c375655b20034b7c0fee019152fae
Kevin Manase <gasydev@gmail.com>
2023-12-07 17:22:36 -0600

    Update base URL in getter.ts

--- src/app/getter.ts
+++ src/app/getter.ts
@@ -1,7 +1,7 @@
 
 import { type TinypediaAnswer } from "./type";
 
-const baseUrl = process.env.NEXT_PUBLIC_ENV === 'local' ? 'http://localhost:3001' : "https://tinypedia-d04xa503p-kevb10.vercel.app";
+const baseUrl = process.env.NEXT_PUBLIC_ENV === 'local' ? 'http://localhost:3001' : "https://tinypedia.vercel.app";
 
 export async function getAnswer(question: string): Promise<string> {
 	const response = await fetch(baseUrl + "/api/ai", {
