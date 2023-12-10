828add775c1239bd285b9391919d26a7805e86b4
Kevin Manase <gasydev@gmail.com>
2023-12-10 14:59:54 -0600

    Add @vercel/analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Inter } from "next/font/google";
 
@@ -21,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
