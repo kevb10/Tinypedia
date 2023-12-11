ec3aca9412c9755ba1cda008d552880f87053626
Kevin Manase <gasydev@gmail.com>
2023-12-11 16:31:23 -0600

    Add opengraph-image component and generateStaticParams and generateMetadata functions

--- src/app/q/[id]/page.tsx
+++ src/app/q/[id]/page.tsx
@@ -1,5 +1,34 @@
+import { type QA } from "~/app/types";
 import { baseUrl } from "~/app/utils";
 import Card from "~/components/card";
+
+// Return a list of `params` to populate the [id] dynamic segment
+export async function generateStaticParams() {
+  const QAs = await fetch(baseUrl + "/api/db").then((res) => res.json());
+
+  return QAs.map((QA: QA) => ({
+    id: QA.id,
+  }));
+}
+
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const qa = await fetch(baseUrl + `/api/db?id=${id}`).then((res) =>
+    res.json(),
+  );
+
+  return {
+    title: `Tinypedia - ${qa.question}`,
+    description: qa.answer,
+    googleBot: {
+      index: true,
+      follow: false,
+    },
+  };
+}
 
 export default async function QuestionPage({
   params: { id },
